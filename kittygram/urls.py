c9275d4f13c1d7c75f14d30bea3ea0f3941ffd76
Borislav Pecherskikh <borislavtarget@gmail.com>
2023-02-22 12:05:00 +0500

    123

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,29 @@
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter, DefaultRouter
 
-from cats.views import cat_list
+from cats.views import APICat, CatDetail, CatList, CatViewSet, cat_list, hello
+
+router = SimpleRouter()
+
+router.register('cats', CatViewSet, basename='tiger')
+
+# urlpatterns = [
+#    path('cats/', CatList.as_view()),
+#    path('hello/', CatDetail.as_view()),
+#    path('gigachad/', CatViewSet.as_view({
+#       'get': 'list',
+#       'post': 'create'
+#    })),
+# ]
 
 urlpatterns = [
-   path('cats/', cat_list),
+   path('', include(router.urls)),
 ]
 
+router = DefaultRouter()
 
+router.register('cats', CatViewSet)
+
+urlpatterns = [
+   path('', include(router.urls)),
+]
